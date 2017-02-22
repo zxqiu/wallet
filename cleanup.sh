b9d1e18688797b380647f4292f1c678cf24d5ca1
zxqiu <zxqiu90@gmail.com>
2017-02-22 15:16:57 -0800

    setup scripts

---
+++ cleanup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+MYSQL_PACKAGE=mysql-5.7.17-linux-glibc2.5-i686
+MYSQL_GROUPNAME=mysql
+MYSQL_USERNAME=zxqiu
+SYSTEM_USERNAME=zxqiu
+CODE_PATH=$(pwd);
+
+
+confirm() {
+    # call with a prompt string or use a default
+    read -r -p "${1:-This opreation will delete all the data in MySql Database. Are you sure? [yes/no]} " response
+    case "$response" in
+        [yY][eE][sS]) 
+            clean
+            ;;
+        *)
+            echo "Smart choice! You just successfully avoid being fired."
+            exit;
+            ;;
+    esac
+}
+
+clean() {
+    echo "Crashing everything in your life ..."
+    sudo rm -rf /usr/local/mysql*
+    sudo rm -rf /etc/init.d/mysql.server
+    sudo rm -rf /etc/profile.d/mysql.sh
+    sudo rm -rf /etc/my.cnf
+    echo "Done."
+}
+
+
+
+confirm
+
--- setup.sh
+++ setup.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
-MYSQL_PACKAGE=mysql-5.7.17-linux-glibc2.5-i686
+MYSQL_PACKAGE=$(pwd)/mysql-5.7.17-linux-glibc2.5-i686.tar.gz
+MYSQL_VERSION=${MYSQL_PACKAGE%.tar*}
+MYSQL_VERSION=${MYSQL_VERSION##*/}
+echo $MYSQL_VERSION
+exit
+
 MYSQL_GROUPNAME=mysql
 MYSQL_USERNAME=zxqiu
 SYSTEM_USERNAME=zxqiu
-CODE_PATH=$(sudo pwd);
+CODE_PATH=$(pwd)
+MYSQL_PATH=/bin/local
 
-echo $CODE_PATH;
+read -r -p "${1:-Make sure you have setup the script correctly. Make sure you have mysql server 5.7.6 or higher. [y/n]} " response
+case "$response" in
+    [yY][eE][sS]|[yY])
+        ;;
+    *)
+        exit;
+        ;;
+esac
 
 sudo yum install libaio;
 
 sudo groupadd $MYSQL_GROUPNAME
 sudo useradd -r -g $MYSQL_GROUPNAME -s /bin/false mysql
 sudo usermod -a -G $MYSQL_GROUPNAME $MYSQL_USERNAME
-cd /usr/local
-sudo tar zxvf $CODE_PATH/$MYSQL_PACKAGE.tar.gz
-sudo ln -s $MYSQL_PACKAGE /usr/local/mysql
+cd MYSQL_PATH
+sudo tar zxvf $CODE_PATH/$MYSQL_PACKAGE
+sudo ln -s $MYSQL_VERSION MYSQL_PATH/mysql
 cd mysql
 sudo mkdir mysql-files
 sudo chmod 750 mysql-files
@@ -23,11 +36,30 @@ sudo chown -R $MYSQL_USERNAME .
 sudo chgrp -R $MYSQL_GROUPNAME .
 #scripts/mysql_install_db --user=mysql# MySQL 5.7.0 to 5.7.4
 #bin/mysql_install_db --user=mysql    # MySQL 5.7.5
-sudo bin/mysqld --initialize --user=$MYSQL_USERNAME # MySQL 5.7.6 and up
+sudo bin/mysqld --initialize-insecure --user=$MYSQL_USERNAME # MySQL 5.7.6 and up
 #sudo bin/mysql_ssl_rsa_setup              # MySQL 5.7.6 and up
 sudo chown -R $SYSTEM_USERNAME .
 sudo chown -R $MYSQL_USERNAME data
 sudo chown -R $MYSQL_USERNAME mysql-files
-sudo bin/mysqld_safe --user=$MYSQL_USERNAME &
-# Next command is optional
+#bin/mysqld_safe --user=$MYSQL_USERNAME &
+
+
+# setup configurations
+echo "[mysqld]" > my.cnf
+echo "datadir="$MYSQL_PATH"/mysql/data" >> my.cnf
+echo "socket=/tmp/mysql.sock" >> my.cnf
+echo "symbolic-links=0" >> my.cnf
+echo "[mysqld_safe]" >> my.cnf
+sudo mv my.cnf /etc/my.cnf
+
+echo "export PATH="$(pwd)"/bin/:$PATH" > mysql.sh
+sudo mv mysql.sh /etc/profile.d/
+
+# start mysql server and init root user
+killall mysqld
+support-files/mysql.server start
+bin/mysqladmin -u root password root
 sudo cp support-files/mysql.server /etc/init.d/mysql.server
+
+# create wallet database for project
+bin/mysql -u root --password=root --execute="create database wallet"
