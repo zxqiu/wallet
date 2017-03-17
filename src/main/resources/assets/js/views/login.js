033c6758d1ce5d88df5ca4d8544fa568047091cd
zxqiu <Naisrep007>
2017-03-17 00:32:33 -0400

    facebook login

--- src/main/resources/assets/js/views/login.js
+++ src/main/resources/assets/js/views/login.js
@@ -2,6 +2,27 @@
  * Created by neo on 3/15/17.
  */
 
+function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+        console.log(response);
+    });
+}
+
+$(document).ready(function() {
+    $.ajaxSetup({ cache: true });
+    $.getScript('//connect.facebook.net/en_US/sdk.js', function(){
+        FB.init({
+            appId: '{your-app-id}',
+            version: 'v2.7' // or v2.1, v2.2, v2.3, ...
+        });
+        $('#loginbutton,#feedbutton').removeAttr('disabled');
+        FB.getLoginStatus(function(response) {
+            console.log(response);
+        });
+    });
+});
+
+
 var form = document.getElementById("loginForm");
 form.noValidate = true;
 $("#loginSubmit").on("click", function() {
