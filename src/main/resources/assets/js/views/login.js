7c3ac67c3cdc289d48bd627df674bde8c406762b
zxqiu <Naisrep007>
2017-03-17 01:56:56 -0400

    fb

--- src/main/resources/assets/js/views/login.js
+++ src/main/resources/assets/js/views/login.js
@@ -5,8 +5,6 @@ function loginSuccess() {
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
         console.log('Successful login for: ' + response.name);
-        document.getElementById('status').innerHTML =
-                    'Thanks for logging in, ' + response.name + '!';
         console.log(response);
     });
 }
