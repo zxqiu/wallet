a2516e70a2f6a297cc85915ff124e5f789af9b67
zxqiu <Naisrep007>
2017-03-17 00:40:25 -0400

    push

--- push2GCE.sh
+++ push2GCE.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gcloud compute copy-files ~/src/wallet/target/wallet-0.0.1-SNAPSHOT.jar instance-wallet:~/wallet --zone us-central1-c
+gcloud compute copy-files ~/src/wallet/target/wallet-0.0.1-SNAPSHOT.jar instance-wallet:~/wallet/target --zone us-central1-c
 gcloud compute copy-files ~/src/wallet/wallet.yml instance-wallet:~/wallet --zone us-central1-c
 gcloud compute copy-files ~/src/wallet/start.sh instance-wallet:~/wallet --zone us-central1-c
