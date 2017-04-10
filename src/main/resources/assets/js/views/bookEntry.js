31703f6f17b3a706883ab713cb16e38b370e47de
zxqiu <zxqiu90@gmail.com>
2017-04-10 10:33:11 -0700

    compress image

--- src/main/resources/assets/js/views/bookEntry.js
+++ src/main/resources/assets/js/views/bookEntry.js
@@ -67,6 +67,32 @@ function setCategoryOptionFontColor() {
     adjustFontColor(bright, parent);
 }
 
+function compress(source_img_obj, quality, maxWidth, output_format){
+    var mime_type = "image/jpeg";
+    if(typeof output_format !== "undefined" && output_format=="png"){
+        mime_type = "image/png";
+    }
+
+    maxWidth = maxWidth || 1000;
+    var natW = source_img_obj.naturalWidth;
+    var natH = source_img_obj.naturalHeight;
+    var ratio = natH / natW;
+    if (natW > maxWidth) {
+        natW = maxWidth;
+        natH = ratio * maxWidth;
+    }
+
+    var cvs = document.createElement('canvas');
+    cvs.width = natW;
+    cvs.height = natH;
+
+    var ctx = cvs.getContext("2d").drawImage(source_img_obj, 0, 0, natW, natH);
+    var newImageData = cvs.toDataURL(mime_type, quality/100);
+    var result_image_obj = new Image();
+    result_image_obj.src = newImageData;
+    return result_image_obj;
+}
+
 /************************** jquery functions ********************************/
 $(document).ready(function () {
     // this is to init category text field
@@ -182,6 +208,7 @@ $("#bookEntryPhoto").on("change", function(e) {
     reader.onload = function(event) {
         var img = new Image();
         img.onload = function(){
+            //compress(img, 30, 300, "jpg");
             var scale = 300 / img.height;
             var height = img.height * scale;
             var width = img.width * scale;
